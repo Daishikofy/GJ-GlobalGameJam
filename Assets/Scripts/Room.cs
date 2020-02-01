800cfd1c6efe36d8706f25217a39da7e09fe1a14
Daishikofy <daishi.setsuko@gmail.com>
2020-01-31 21:18:43 -0300

    Added resources folder and roomsPrefab subFolder

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,18 @@
+﻿#pragma warning disable 0649
+using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour
+{
+    [HideInInspector]
+    public bool isEntry;
+    [HideInInspector]
+    public bool isExit;
+    [SerializeField]
+    RoomType type;
+
+    public RoomType Type
+    {
+        get { return type; }
+    }
+}
