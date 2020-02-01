b526ab8b358706f33c6c1afd18171ce3aa34bde5
Daishikofy <daishi.setsuko@gmail.com>
2020-02-01 17:01:16 -0300

    Created a camera controler

---
+++ Assets/Scripts/Controller/CameraController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private static CameraController instance;
+    int currentPos;
+    public int position;
+
+    Camera camera;
+
+    #region Singleton
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+            Destroy(this.gameObject);
+        else
+            instance = this;
+    }
+
+    public static CameraController Instance { get { return instance; } }
+    #endregion Singleton
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        currentPos = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerRelativePos = camera.WorldToViewportPoint(GameManager.Instance.player.transform.position);
+        if (playerRelativePos.x > 1)
+            moveHorizontal(1);
+        else if (playerRelativePos.x < 0)
+            moveHorizontal(-1);
+        else if (playerRelativePos.y > 1)
+            moveVertical(1);
+        else if (playerRelativePos.y < 0)
+            moveVertical(-1);
+    }
+
+    public void setPosition(Vector3 position)
+    {
+        transform.position = position;       
+    }
+
+    public void moveHorizontal(int value)
+    {
+        transform.position += Vector3.right * value * DungeonFactory.Instance.roomMaxDimensions.x;
+    }
+    public void moveVertical(int value)
+    {
+        transform.position += Vector3.up * value * DungeonFactory.Instance.roomMaxDimensions.y;
+    }
+}
